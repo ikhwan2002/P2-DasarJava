6eb69c7f1713e70395f3370ce5ef43cd61e9a647
DELL <rizkiakmal37@gmail.com>
2022-03-21 20:42:47 +0700

    Mengguanakan switch-case

---
+++ src/P2Q_PercabanganSwitch.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class P2Q_PercabanganSwitch {
+    public static void main(String[] args) {
+
+        //membuat variabel dan Scanner
+        String lampu;
+        Scanner scan = new Scanner(System.in);
+
+        //mengambil input
+        System.out.println("Inputkan nama warna: ");
+        lampu = scan.nextLine();
+
+        switch (lampu){
+            case "merah":
+                System.out.println("Lampu merah,berhenti!");
+                break;
+            case "kuning":
+                System.out.println("Lampu kuning,harap hati-hati!");
+                break;
+            case "hijau":
+                System.out.println("Lampu hijau,silahkan jalan!");
+                break;
+            default:
+                System.out.println("Warna lampu salah!");
+        }
+    }
+}
