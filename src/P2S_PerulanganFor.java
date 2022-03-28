eed0925702a1c133e6205c7f992aba510364d66e
DELL <rizkiakmal37@gmail.com>
2022-03-28 13:59:30 +0700

    Menggunakan perulangan for

---
+++ src/P2S_PerulanganFor.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class P2S_PerulanganFor {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukkan jumlah perulangan : ");
+        int jml = input.nextInt();
+        for (int i = 0; i < jml; i++)
+            System.out.println("Pemrograman Berorientasi Objek dengan Java!");
+    }
+}
